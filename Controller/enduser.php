daa826415a046fff400ff1d3fa606f8b9953a511
sumeet5090 <58212406+sumeet5090@users.noreply.github.com>
2021-03-07 17:41:38 +0530

    made a basic file structure

---
+++ Controller/enduser.php
@@ -0,0 +1,7 @@
+<?php
+
+if(!defined('CAR')){
+    die('HACKING ATTEMPT');
+}
+
+include_once('./Library/session.php');
--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
 
-echo "Hi";
+define('CAR', 1);
+
+if($_SERVER['SERVER_PORT'] == '2777'){
+    include_once('./Controller/admin.php');
+}else {
+    include_once('./Controller/enduser.php');
+}
